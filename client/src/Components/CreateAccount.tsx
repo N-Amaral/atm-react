cffed72385299851aef0498100eb992d4a336c9e
NAmaral <26823958+N-Amaral@users.noreply.github.com>
2022-10-18 23:41:30 +0100

    Update card creation and validation functions. Should work now.

--- client/src/Components/CreateAccount.tsx
+++ client/src/Components/CreateAccount.tsx
@@ -10,32 +10,45 @@ const CardList = () => {
 };
 
 const CreateAccount = () => {
-  function makeCardNum(cardNum: number[]) {
-    while (cardNum.length !== 21) {
-      const num: number = Math.floor(Math.random() * 10);
-      cardNum.push(num);
-    }
+  function makeCardNum() {
+    const ind: number = Math.floor(Math.random() * 23);
+    const bankNum: string[] = bankIds.bankNum;
+    let tempNum: string = bankNum[ind] + "0000";
+    let restNum: string = "";
 
-    return cardNum;
+    while (restNum.length !== 13) {
+      restNum += Math.floor(Math.random() * 10).toString();
+    }
+    return parseInt((tempNum += restNum));
   }
 
   function verifyCardNum() {
-    const cardNum: number[] = [];
-    const finished = parseInt(cardNum.join(""));
-
-    for (let i = 0; i < 100; i++) {
-      makeCardNum(cardNum);
-      if (finished % 97 === 1) {
-        console.log(finished);
-      }
-      console.log(i);
+    let tempCardNum: number = makeCardNum();
+    let fCardNum: string = "";
+    while (tempCardNum % 97 !== 1) {
+      tempCardNum = makeCardNum();
     }
+    if (tempCardNum.toString().length === 21) {
+      fCardNum = tempCardNum.toString();
+    }
+    if (tempCardNum.toString().length <= 19) {
+      fCardNum = "00" + tempCardNum.toString();
+    } else {
+      fCardNum = "0" + tempCardNum.toString();
+    }
+    return fCardNum;
   }
 
   function createCard() {
-    const cardNum = [];
+    let card = verifyCardNum();
+    if (card.length > 21) {
+      card = verifyCardNum();
+    } else {
+      console.log(card.length);
+    }
   }
-  verifyCardNum();
+
+  createCard();
   return (
     <>
       <div className="cardList-wrapper"></div>
